068d705a77eec9616530e4269eeb10cf6c6b0006
Ricky Justiniano <justiniano.ricky@gmail.com>
2020-12-16 11:30:49 -0700

    delete folder

--- questions/color.js
+++
@@ -1,6 +0,0 @@
-const chalk = require("chalk");
-exports.VIEW = chalk.underline.redBright("View");
-exports.EMPLOYEE = chalk.underline.magentaBright("Employee");
-exports.DEPARTMENT = chalk.underline.blueBright("Department");
-exports.ROLE = chalk.underline.yellowBright("Role");
-exports.UPDATEEMP = chalk.underline.cyanBright("Update Employee Role");
